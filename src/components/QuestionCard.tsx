d200a5d5b14355ee48faaf63ebe57e8355454db1
huangjinyuan <156567947@qq.com>
2024-07-30 23:42:03 +0800

    8-11 le

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,22 +1,81 @@
-import React from 'react'
-
-import s from './Question.module.scss'
+import s from "./Question.module.scss";
+import { Button, Space, Divider, Tag } from "antd";
+import {
+  CopyOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  LineChartOutlined,
+  StarOutlined,
+} from "@ant-design/icons";
+import { useNavigate, Link } from "react-router-dom";
 
 type PropsType = {
-  _id: string,
-  title: string,
-  isStar: boolean,
-  isPublished: boolean,
-  answerCount: number,
-  createdAt: string,
-}
+  _id: string;
+  title: string;
+  isStar: boolean;
+  isPublished: boolean;
+  answerCount: number;
+  createdAt: string;
+};
 export default function QuestionCard(props: PropsType) {
-  const { _id, title } = props
+  const { _id, title, createdAt, answerCount, isPublished, isStar } = props;
+  const nav = useNavigate();
   return (
-    <>
-      <h1>{title}</h1>
-      <button>编辑问卷</button>
-      <button>数据统计</button>
-    </>
-  )
+    <div className={s.container}>
+      <div className={s.title}>
+        <div className={s.left}>
+          <Link
+            to={isPublished ? `/question/stat/${_id}` : `/question/edit/${_id}`}
+          >
+            <Space>
+              {isStar && <StarOutlined style={{ color: "red" }} />}
+              {title}
+            </Space>
+          </Link>
+        </div>
+        <div className={s.right}>
+          <Space>
+            {isPublished ? (
+              <Tag color="blue">已发布</Tag>
+            ) : (
+              <Tag>未发布</Tag>
+            )}
+            <span>答卷：{answerCount}</span>
+            <span>{createdAt}</span>
+          </Space>
+        </div>
+      </div>
+      <Divider style={{margin:'12px'}} />
+      <div className={s["button-container"]}>
+        <div className={s.left}>
+          <Space>
+            <Button
+              icon={<EditOutlined />}
+              type="text"
+              onClick={() => nav(`/question/edit/${_id}`)}
+            >
+              编辑问卷
+            </Button>
+            <Button
+              icon={<LineChartOutlined />}
+              type="text"
+              onClick={() => nav(`/question/stat/${_id}`)}
+              disabled={!isPublished}
+            >
+              数据统计
+            </Button>
+          </Space>
+        </div>
+        <div className={s.right}>
+          <Space>
+            <Button icon={<StarOutlined />}>
+              {isStar ? "取消标星" : "标星"}
+            </Button>
+            <Button icon={<CopyOutlined />}>复制</Button>
+            <Button icon={<DeleteOutlined />}>删除</Button>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/page/manage/List.tsx
+++ src/page/manage/List.tsx
@@ -1,25 +1,49 @@
-import { useState } from 'react'
-import QuestionCard from '@/components/QuestionCard.tsx'
-import styles from './List.module.scss'
+import { useState } from "react";
+import QuestionCard from "@/components/QuestionCard.tsx";
+import styles from "./List.module.scss";
+import { Space } from "antd";
 export default function List() {
   const [list, setList] = useState([
-    { _id: 'q1', title: '问卷1', isPublished: false, isStar: false, answerCouynt: 5, createdAt: '12:52:25' },
-    { _id: 'q2', title: '问卷2', isPublished: false, isStar: true, answerCouynt: 5, createdAt: '12:52:25' },
-    { _id: 'q3', title: '问卷3', isPublished: true, isStar: false, answerCouynt: 5, createdAt: '12:52:25' },
-  ])
+    {
+      _id: "q1",
+      title: "问卷1",
+      isPublished: false,
+      isStar: false,
+      answerCount: 5,
+      createdAt: "12:52:25",
+    },
+    {
+      _id: "q2",
+      title: "问卷2",
+      isPublished: false,
+      isStar: true,
+      answerCount: 5,
+      createdAt: "12:52:25",
+    },
+    {
+      _id: "q3",
+      title: "问卷3",
+      isPublished: true,
+      isStar: false,
+      answerCount: 5,
+      createdAt: "12:52:25",
+    },
+  ]);
   return (
-
     <>
       <div className={styles.header}>
-        <div className={styles.left}><h3>我的问卷</h3></div>
+        <div className={styles.left}>
+          <h3>我的问卷</h3>
+        </div>
         <div className={styles.right}>搜索 </div>
       </div>
-      <div>{list.map(item=><QuestionCard key={item._id} {...item} />)}</div>
-      <div>footer</div>
+      <div style={{ width:'100%' }}>
+        <Space direction="vertical">
+          {list.length > 0 &&
+            list.map((item) => <QuestionCard key={item._id} {...item} />)}
+        </Space>
+      </div>
+      <div className={styles.footer}>加载更多</div>
     </>
-
-
-
-
-  )
+  );
 }
